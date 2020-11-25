37401ecdd2f18d78defcd6cda3f0a3995c5adfc2
Rikus Louw <rikuswlouw@gmail.com>
2020-11-25 12:33:11 +0200

    Added unitSpawner

--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -5,5 +5,12 @@ using UnityEngine;
 
 public class RTSNetworkManager : NetworkManager
 {
-    
+    [SerializeField] private GameObject unitSpawnerPrefab = null;
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        GameObject unitSpawnerInstance = Instantiate(unitSpawnerPrefab, conn.identity.transform.position, conn.identity.transform.rotation);
+        NetworkServer.Spawn(unitSpawnerInstance, conn);
+    }
 }
