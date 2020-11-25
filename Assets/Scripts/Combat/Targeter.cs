e9181a539ff3be60c295e6bb86bcc874beff42e8
Rikus Louw <rikuswlouw@gmail.com>
2020-11-25 15:22:59 +0200

    Added Targeting

---
+++ Assets/Scripts/Combat/Targeter.cs
@@ -0,0 +1,26 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Targeter : NetworkBehaviour
+{
+    private Targetable target;
+
+    #region Server
+    [Command]
+    public void CmdSetTarget(GameObject targetGameObject)
+    {
+        if (!targetGameObject.TryGetComponent<Targetable>(out Targetable target))
+            return;
+
+        this.target = target;
+    }
+
+    [Server]
+    public void ClearTarget()
+    {
+        this.target = null;
+    }
+    #endregion
+}
